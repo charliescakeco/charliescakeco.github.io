4af4c54c7fffc3b432e5bdef9cabf9f407e87026
charliescakeco <charliescakeco@gmail.com>
2020-07-08 20:58:35 -0400

    slight landing page update

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+	<div id="app">
+		<!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+		<HelloWorld msg="Welcome to Charlie's Cake Co."/>
+	</div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+	name: 'App',
+	components: {
+		HelloWorld
+	}
+}
+</script>
+
+<style>
+	
+#app {
+
+	height: 100%;
+
+}
+
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,318 @@
+<template>
+
+	<div id="wrapper">
+		
+		<div id="splash-container" class="stage">
+		
+			<p class="title">Charlies Cake Co.</p>
+			<p class="info">Coming soon!!<br>Stay tuned for details...</p>
+
+		</div>
+
+		<footer id="footer-content" class="active">
+
+			<p id="copy-date"><span>&copy;&nbsp;</span>2020</p>
+
+			<div class="social-link-wrapper">
+				<a href="mailto:charliescakeco@gmail.com">
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" style="fill: rgb(0, 0, 0)">
+						<title>mail</title>
+						<path d="M15.996 15.457l16.004-7.539v-3.918h-32v3.906zM16.004 19.879l-16.004-7.559v15.68h32v-15.656z"></path>
+					</svg>
+				</a>
+				<!-- <a href="https://www.facebook.com/" target="_blank">
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" style="fill: rgb(0, 0, 0)">
+						<title>facebook</title>
+						<path d="M19 6h5v-6h-5c-3.86 0-7 3.14-7 7v3h-4v6h4v16h6v-16h5l1-6h-6v-3c0-0.542 0.458-1 1-1z"></path>
+					</svg>
+				</a> -->
+				<a href="https://www.instagram.com/charliescakeco/" target="_blank">
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" style="fill: rgb(0, 0, 0)">
+						<title>instagram</title>
+						<path d="M16 2.881c4.275 0 4.781 0.019 6.462 0.094 1.563 0.069 2.406 0.331 2.969 0.55 0.744 0.288 1.281 0.638 1.837 1.194 0.563 0.563 0.906 1.094 1.2 1.838 0.219 0.563 0.481 1.412 0.55 2.969 0.075 1.688 0.094 2.194 0.094 6.463s-0.019 4.781-0.094 6.463c-0.069 1.563-0.331 2.406-0.55 2.969-0.288 0.744-0.637 1.281-1.194 1.837-0.563 0.563-1.094 0.906-1.837 1.2-0.563 0.219-1.413 0.481-2.969 0.55-1.688 0.075-2.194 0.094-6.463 0.094s-4.781-0.019-6.463-0.094c-1.563-0.069-2.406-0.331-2.969-0.55-0.744-0.288-1.281-0.637-1.838-1.194-0.563-0.563-0.906-1.094-1.2-1.837-0.219-0.563-0.481-1.413-0.55-2.969-0.075-1.688-0.094-2.194-0.094-6.463s0.019-4.781 0.094-6.463c0.069-1.563 0.331-2.406 0.55-2.969 0.288-0.744 0.638-1.281 1.194-1.838 0.563-0.563 1.094-0.906 1.838-1.2 0.563-0.219 1.412-0.481 2.969-0.55 1.681-0.075 2.188-0.094 6.463-0.094zM16 0c-4.344 0-4.887 0.019-6.594 0.094-1.7 0.075-2.869 0.35-3.881 0.744-1.056 0.412-1.95 0.956-2.837 1.85-0.894 0.888-1.438 1.781-1.85 2.831-0.394 1.019-0.669 2.181-0.744 3.881-0.075 1.713-0.094 2.256-0.094 6.6s0.019 4.887 0.094 6.594c0.075 1.7 0.35 2.869 0.744 3.881 0.413 1.056 0.956 1.95 1.85 2.837 0.887 0.887 1.781 1.438 2.831 1.844 1.019 0.394 2.181 0.669 3.881 0.744 1.706 0.075 2.25 0.094 6.594 0.094s4.888-0.019 6.594-0.094c1.7-0.075 2.869-0.35 3.881-0.744 1.050-0.406 1.944-0.956 2.831-1.844s1.438-1.781 1.844-2.831c0.394-1.019 0.669-2.181 0.744-3.881 0.075-1.706 0.094-2.25 0.094-6.594s-0.019-4.887-0.094-6.594c-0.075-1.7-0.35-2.869-0.744-3.881-0.394-1.063-0.938-1.956-1.831-2.844-0.887-0.887-1.781-1.438-2.831-1.844-1.019-0.394-2.181-0.669-3.881-0.744-1.712-0.081-2.256-0.1-6.6-0.1v0z"></path>
+						<path d="M16 7.781c-4.537 0-8.219 3.681-8.219 8.219s3.681 8.219 8.219 8.219 8.219-3.681 8.219-8.219c0-4.537-3.681-8.219-8.219-8.219zM16 21.331c-2.944 0-5.331-2.387-5.331-5.331s2.387-5.331 5.331-5.331c2.944 0 5.331 2.387 5.331 5.331s-2.387 5.331-5.331 5.331z"></path>
+						<path d="M26.462 7.456c0 1.060-0.859 1.919-1.919 1.919s-1.919-0.859-1.919-1.919c0-1.060 0.859-1.919 1.919-1.919s1.919 0.859 1.919 1.919z"></path>
+					</svg>
+				</a>
+				<!-- <a href="https://www.youtube.com/channel/UCwe3mHk1WryEY5LAmX7WuLA" target="_blank">
+					<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" style="fill: rgb(0, 0, 0)">
+						<title>youtube</title>
+						<path d="M31.681 9.6c0 0-0.313-2.206-1.275-3.175-1.219-1.275-2.581-1.281-3.206-1.356-4.475-0.325-11.194-0.325-11.194-0.325h-0.012c0 0-6.719 0-11.194 0.325-0.625 0.075-1.987 0.081-3.206 1.356-0.963 0.969-1.269 3.175-1.269 3.175s-0.319 2.588-0.319 5.181v2.425c0 2.587 0.319 5.181 0.319 5.181s0.313 2.206 1.269 3.175c1.219 1.275 2.819 1.231 3.531 1.369 2.563 0.244 10.881 0.319 10.881 0.319s6.725-0.012 11.2-0.331c0.625-0.075 1.988-0.081 3.206-1.356 0.962-0.969 1.275-3.175 1.275-3.175s0.319-2.587 0.319-5.181v-2.425c-0.006-2.588-0.325-5.181-0.325-5.181zM12.694 20.15v-8.994l8.644 4.513-8.644 4.481z"></path>
+					</svg>
+				</a> -->
+			</div>
+
+		</footer>
+
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'HelloWorld',
+	props: {
+		msg: String
+	}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+:root {
+	
+	--main-font-color: rgba(0, 0, 0, 1) !important;
+	--footer-background-color: rgba(245,240,240, 1);
+
+}
+
+@font-face {
+	font-family: 'Dancing-Script-Variable';
+	src: url('../resources/fonts/Dancing_Script/DancingScript-VariableFont_wght.ttf') format('truetype');
+	font-weight: normal;
+	font-style: normal;
+}
+
+@font-face{ 
+	font-family: 'Martel-Black';
+	src: url('../resources/fonts/Martel/Martel-Black.ttf') format('truetype');
+}
+
+@font-face{ 
+	font-family: 'Martel-Bold';
+	src: url('../resources/fonts/Martel/Martel-Bold.ttf') format('truetype');
+}
+
+@font-face{ 
+	font-family: 'Martel-ExtraBold';
+	src: url('../resources/fonts/Martel/Martel-ExtraBold.ttf') format('truetype');
+}
+
+@font-face{ 
+	font-family: 'Martel-ExtraLight';
+	src: url('../resources/fonts/Martel/Martel-ExtraLight.ttf') format('truetype');
+}
+
+@font-face{ 
+	font-family: 'Martel-Light';
+	src: url('../resources/fonts/Martel/Martel-Light.ttf') format('truetype');
+}
+
+@font-face{ 
+	font-family: 'Martel-Regular';
+	src: url('../resources/fonts/Martel/Martel-Regular.ttf') format('truetype');
+}
+
+@font-face{ 
+	font-family: 'Martel-SemiBold';
+	src: url('../resources/fonts/Martel/Martel-SemiBold.ttf') format('truetype');
+}
+
+@keyframes pulse_animation {
+	0% { transform: scale(1); }
+	50% { transform: scale(1.08); }
+	100% { transform: scale(1); }
+}
+
+body, 
+html {
+
+	height: 100%;
+	width: 100vw;
+	min-width: 250px;
+	width: auto;
+	overflow: hidden;
+	background-color: #fff;
+	font-family: 'Dancing-Script-Variable', 'Martel-Regular', 'Helvetica', 'Open-Sans';
+	font-weight: 125 950;
+	font-stretch: 75% 125%;
+	font-style: oblique 0deg 20deg;
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-box-orient: vertical;
+	-webkit-box-direction: normal;
+	-ms-flex-direction: column;
+	flex-direction: column;
+	-webkit-box-pack: end;
+	-ms-flex-pack: end;
+	justify-content: flex-end;
+	-webkit-box-align: center;
+	-ms-flex-align: center;
+	align-items: center;
+	margin: 0 auto;
+
+
+}
+
+#wrapper {
+
+	height: inherit;
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-box-orient: vertical;
+	-webkit-box-direction: normal;
+	-ms-flex-direction: column;
+	flex-direction: column;
+	-webkit-box-flex: 1;
+	-ms-flex: 1 1 auto;
+	flex: 1 1 auto;
+
+}
+
+.stage {
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: flex;
+	width: 100%;
+}
+
+.box {
+	animation-duration: 2s;
+	animation-iteration-count: infinite;
+}
+@keyframes bounce-7 {
+	0%   { 
+		transform: scale(1,1)    
+		translateY(0); 
+	}
+	10%  { 
+		transform: scale(1.1,.9) 
+		translateY(0); 
+	}
+	30%  { 
+		transform: scale(.9,1.1) 
+		translateY(-100px); 
+	}
+	50%  { 
+		transform: scale(1.05,.95) 
+		translateY(0); 
+	}
+	57%  { 
+		transform: scale(1,1) 
+		translateY(-7px); 
+	}
+	64%  { 
+		transform: scale(1,1) 
+		translateY(0); 
+	}
+	100% { 
+		transform: scale(1,1) 
+		translateY(0); 
+	}
+}
+
+#splash-container {
+	display: -webkit-box;
+	display: -ms-flexbox;
+	display: flex;
+	-webkit-box-orient: vertical;
+	-webkit-box-direction: normal;
+	-ms-flex-direction: column;
+	flex-direction: column;
+	-webkit-box-align: center;
+	-ms-flex-align: center;
+	align-items: center;
+	-webkit-box-pack: center;
+	-ms-flex-pack: center;
+	justify-content: center;
+	width: 100vw;
+	-webkit-box-flex: 0;
+	-ms-flex: 0 1 auto;
+	flex: 0 1 auto;
+	overflow: scroll;
+	height: 100%;
+}
+
+#splash-container p {
+	margin: 0.25rem 1rem;
+	text-align: center;
+}
+
+#splash-container p.title {
+	font-family: 'Dancing-Script-Variable';
+	font-size: 2.25rem;
+}
+
+#splash-container p.info {
+	font-family: 'Martel-Regular';
+	font-size: 1rem;
+}
+
+@media screen and (max-width: 992px){
+	
+	#splash-container p.title {
+		font-size: 1.5rem;
+	}
+
+	#splash-container p.info {
+		font-size: 0.5rem;
+	}
+
+
+}
+
+#footer-content {
+
+	color: rgba(0, 0, 0, 1);
+	font-size: 0.75rem;
+	width: 100vw;
+	border: none;
+	background-color: transparent;
+	padding: 1.25rem 0rem;
+	display: none;
+	/*-webkit-box-orient: vertical;
+	-webkit-box-direction: normal;
+	-ms-flex-direction: column;
+	flex-direction: column;*/
+	-webkit-box-pack: center;
+	-ms-flex-pack: center;
+	justify-content: center;
+	flex: 0 1 auto;
+
+}
+
+#footer-content.active {
+
+    display: -webkit-box;
+    display: -ms-flexbox;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    background-color: aliceblue;
+}
+
+.social-link-wrapper {
+
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+
+}
+
+.social-link-wrapper a {
+
+	text-decoration: none;
+	display: inline-block;
+
+}
+
+.social-link-wrapper a:hover {
+
+	animation-name: pulse_animation;
+	animation-duration: 2000ms;
+	transform-origin: 60% 60%;
+	animation-iteration-count: infinite;
+	animation-timing-function: linear;
+
+}
+
+.social-link-wrapper a svg {
+
+	padding: 0rem 0.5rem;
+	height: 1.25rem;
+	width: auto;
+
+}
+</style>
